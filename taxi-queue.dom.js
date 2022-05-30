e859ae8c5b2db588f1d45a8103817f3834cd63a8
Ntsakelo <Ntsakeloarnold00@gmail.com>
2022-05-30 13:21:55 +0200

    Taxi app project

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,11 +1,63 @@
 // write your DOM code here.
 
-
 // DOM element references
+//Passengers
+const passengerCount = document.querySelector(".passenger_queue_count");
+const joinBtn = document.querySelector(".join_queue");
+const leaveBtn = document.querySelector(".leave_queue");
 
+//Taxis
+const taxiCount = document.querySelector(".taxi_queue_count");
+const taxiJoinBtn = document.querySelector(".join_taxi_queue");
+
+//Departing
+const departBtn = document.querySelector(".depart");
 // create Factory Function instance
-
 const taxiQueue = TaxiQueue();
 
 // DOM events
+//Retrieve local storage
+if (localStorage["passCount"]) {
+  let newCount = JSON.parse(localStorage.getItem("passCount"));
+  taxiQueue.newPeopleCount(newCount);
+  passengerCount.innerHTML = newCount;
+}
+if (localStorage["taxiCount"]) {
+  let myCount = JSON.parse(localStorage.getItem("taxiCount"));
+  taxiQueue.newTaxiCount(myCount);
+  taxiCount.innerHTML = myCount;
+}
 
+//Passenger join
+joinBtn.addEventListener("click", function () {
+  taxiQueue.joinQueue();
+  passengerCount.innerHTML = taxiQueue.queueLength();
+  //local storage
+  let theCount = taxiQueue.queueLength();
+  stringCount = JSON.stringify(theCount);
+  localStorage.setItem("passCount", stringCount);
+});
+
+//Passenger leave
+leaveBtn.addEventListener("click", function () {
+  taxiQueue.leaveQueue();
+  passengerCount.innerHTML = taxiQueue.queueLength();
+});
+
+//Taxi join
+taxiJoinBtn.addEventListener("click", function () {
+  taxiQueue.joinTaxiQueue();
+  taxiCount.innerHTML = taxiQueue.taxiQueueLength();
+
+  //localStorage
+  let myTaxiCount = taxiQueue.taxiQueueLength();
+  let stringTaxiCount = JSON.stringify(myTaxiCount);
+  localStorage.setItem("taxiCount", stringTaxiCount);
+});
+
+departBtn.addEventListener("click", function () {
+  taxiQueue.taxiDepart();
+
+  taxiCount.innerHTML = taxiQueue.taxiQueueLength();
+  passengerCount.innerHTML = taxiQueue.queueLength();
+});
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,36 +1,49 @@
 function TaxiQueue() {
+  let peopleCount = 0;
+  let taxiCount = 0;
 
+  function joinQueue() {
+    peopleCount += 1;
+  }
 
-	function joinQueue() {
+  function leaveQueue() {
+    if (peopleCount > 0) {
+      peopleCount--;
+    }
+  }
 
-	}
+  function joinTaxiQueue() {
+    taxiCount += 1;
+  }
 
-	function leaveQueue() {
+  function queueLength() {
+    return peopleCount;
+  }
 
-	}
+  function taxiQueueLength() {
+    return taxiCount;
+  }
 
-	function joinTaxiQueue() {
-
-	}
-
-	function queueLength() {
-
-	}
-
-	function taxiQueueLength() {
-
-	}
-
-	function taxiDepart(){
-
-	}
-
-	return {
-		joinQueue,
-		leaveQueue,
-		joinTaxiQueue,
-		queueLength,
-		taxiQueueLength,
-		taxiDepart
-	}
+  function taxiDepart() {
+    if (queueLength() >= 12 && taxiQueueLength() > 0) {
+      taxiCount--;
+      peopleCount -= 12;
+    }
+  }
+  function newPeopleCount(val) {
+    peopleCount = val;
+  }
+  function newTaxiCount(val) {
+    taxiCount = val;
+  }
+  return {
+    joinQueue,
+    leaveQueue,
+    joinTaxiQueue,
+    queueLength,
+    taxiQueueLength,
+    taxiDepart,
+    newPeopleCount,
+    newTaxiCount,
+  };
 }
